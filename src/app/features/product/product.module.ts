bedaf768256f3a92b9facb5d8f105806d2d83de7
IceClockPlus <39346738+IceClockPlus@users.noreply.github.com>
2023-01-13 13:03:06 -0300

    2 files modified, 3 added

--- src/app/features/product/product.module.ts
+++ src/app/features/product/product.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product.routing.module';
 
+//Pages
 import { IndexComponent } from './pages/index/index.component';
+
+//Components
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   imports: [CommonModule, ProductRoutingModule],
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, ProductCardComponent],
 })
 export class ProductModule {}
